4e0e02f2a0121b1020e642e77625153318e7e7c5
Stephen A. Fuqua <stephen@safnet.com>
2022-09-23 08:54:08 -0500

    Again trying to trigger codeql

--- temp.js
+++ temp.js
@@ -3,6 +3,6 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-let a =  1;
-let b = 0;
+const a =  1;
+const b = 0;
 let c = a/b;
